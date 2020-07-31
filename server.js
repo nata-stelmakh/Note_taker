9532deb3285800737acf4e59862eb427cfdcb6e1
Author Name <stelmakh.natallia@gmail.com>
2020-07-31 18:15:58 -0400

    Created HTML routes and variable'data' that reads notes saved injson file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs =require("fs")
 
 // Sets up the Express App
 // =============================================================
@@ -16,3 +17,41 @@ app.listen(PORT, function() {
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
+// The application should have a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
+var data = fs.readFileSync('./db/db.json','utf8')
+console.log(data)
+
+//* GET `*` - Should return the `index.html` file
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+//* GET `/notes` - Should return the `notes.html` file.
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// The following API routes should be created:
+
+//  GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+app.get("/api/notes", function(req, res) {
+  return res.json(data);//?
+});
+//   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+app.post("/api/characters", function(req, res) {
+   
+  var newNote = req.body;
+
+  // newNote.uniqueID = 
+
+  console.log(newNote);
+
+  data.push(newNote);
+
+  res.json(newNote);
+});
+
+//   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
